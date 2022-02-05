9a8803ee859dee1f2a201610c5c258fd26eef680
Manuel Hutter <manuel@hutter.io>
2022-02-05 16:04:46 +0100

    Implement name validation
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -7,6 +7,8 @@ use serde::Deserialize;
 use sqlx::{query, PgPool};
 use uuid::Uuid;
 
+use crate::domain::{NewSubscriber, SubscriberName};
+
 #[derive(Deserialize)]
 pub struct FormData {
     email: String,
@@ -23,22 +25,34 @@ pub struct FormData {
 )]
 #[allow(clippy::async_yields_async)]
 pub async fn subscribe(db: Data<PgPool>, form: Form<FormData>) -> HttpResponse {
-    match insert_subscriber(&db, &form).await {
+    let name = match SubscriberName::parse(form.0.name) {
+        Ok(name) => name,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+    let new_subscriber = NewSubscriber {
+        email: form.0.email,
+        name,
+    };
+
+    match insert_subscriber(&db, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
 
-#[tracing::instrument(name = "Saving subscriber details into DB", skip(db, form))]
-pub async fn insert_subscriber(db: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+#[tracing::instrument(name = "Saving subscriber details into DB", skip(db, new_subscriber))]
+pub async fn insert_subscriber(
+    db: &PgPool,
+    new_subscriber: &NewSubscriber,
+) -> Result<(), sqlx::Error> {
     query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        form.email,
-        form.name,
+        new_subscriber.email,
+        new_subscriber.name.as_ref(),
         Utc::now(),
     )
     .execute(db)
